0b25e66e377f63a6a71f3adba6c415d9515bcaec
tatianamac <github@tatianamac.com>
2021-04-08 02:49:58 -0700

    🚀 Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,46 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Tatiana Mac | v. tatwentyone</title>
       </Head>
-
+      <header>
+        <h1>Tatiana Mac</h1>
+      </header>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <p className={styles.title}>
+          Change is iterative.
+          <br />
+          Growth is incremental.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p className={styles.copy}>
+          That's why I'm building the new tatianamac.com in the open. I wish for
+          more vulnerability in our industry, so who better to start with than
+          myself and my own website?
+        </p>
+        <h2>Principles</h2>
+        <ul className={styles.copy}>
+          <li>
+            <span class={styles.accent}>Performance</span>: I'll only use what I
+            need and load it responsibly.
+          </li>
+          <li>
+            <span class={styles.accent}>Accessibility:</span> I'll make this
+            work for as many people as possible.
+          </li>
+          <li>
+            <span class={styles.accent}>Consent:</span> I'll ask permission for
+            my user's preferences.
+          </li>
+          <li>
+            <span class={styles.accent}>Teaching:</span> I'll document what I
+            learn to pay it forward.
+          </li>
+        </ul>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
